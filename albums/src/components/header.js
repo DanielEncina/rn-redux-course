34d38343ddb7a70932a3e3064d9575be36d331e2
Daniel Encina <dani.encina@alumnos.duoc.cl>
2018-11-11 18:57:28 -0300

    proyect albums ready

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,31 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
+// Make a component
 const Header = (props) => {
   const { textStyle, viewStyle } = styles;
+
   return (
-    <View style={viewStyle} >
+    <View style={viewStyle}>
       <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
+const styles = {
   viewStyle: {
-    backgroundColor: '#f8f8f8',
+    backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 80,
-    paddingTop: 30,
+    height: 60,
+    paddingTop: 15,
     shadowColor: '#000',
-    shadowOffset: { width: 0, heigth: 2 },
-    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
     elevation: 2,
     position: 'relative'
   },
   textStyle: {
     fontSize: 20
   }
-});
+};
 
+// Make the component available to other parts of the app
 export default Header;
